2afebbf0810e09721622c341552ae5bd30a54fca
Ami <ami@dunamu.com>
2023-04-16 12:41:53 +0900

    Updates

---
+++ _next/static/TKGRwflbm017iHy-kWQMz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ba01433ea543f57e.css","static/chunks/pages/index-536308fce1ca6578.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
